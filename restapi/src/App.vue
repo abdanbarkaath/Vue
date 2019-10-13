aa35217d37fac36aed1842650ae30d199d10caf6
abdanbarkaath <abdanbarkaath@fissionlabs.in>
2019-10-13 13:13:18 +0530

    added json file

--- restapi/src/App.vue
+++ restapi/src/App.vue
@@ -1,17 +1,64 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>|
-      <router-link to="/employees">employees</router-link>|
-    </div>
-    <router-view/>
+    <nav class="navbar navbar-expand sticky-top topnav">
+      <a class="navbar-brand" routerLink="mobiles">
+        <img src="https://img.icons8.com/cotton/25/000000/shopping.png" />
+        <span>ShopZone</span>
+      </a>
+      <div class="navbar-collapse collapse">
+        <ul class="navbar-nav pt-1">
+          <li class="nav-item">
+            <div class="dropdown" id="drp-down">
+              <a
+                class="btn btn btn-outline-success dropdown-toggle"
+                href="#"
+                role="button"
+                id="dropdownMenuLink"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >Catogories</a>
+              <div class="dropdown-menu" aria-labelledby="dropdownMenuLink" id="items">
+                <a class="dropdown-item">
+                  <router-link to="/">Home</router-link>
+                </a>
+                <a class="dropdown-item">
+                  <router-link to="/about">About</router-link>
+                </a>
+                <a class="dropdown-item">
+                  <router-link to="/employees">Employees</router-link>
+                </a>
+              </div>
+            </div>
+          </li>
+        </ul>
+        <ul class="nav navbar-nav flex-row ml-auto" id="ul-3">
+          <li class="nav-item">
+            <a routerLink="cart">
+              <button type="button" class="btn btn-light sell-btn">
+                <img src="https://img.icons8.com/pastel-glyph/25/000000/shopping-cart--v2.png" />
+                <p id="cart">Cart</p>
+              </button>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+      <div class="sidenav">
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Clients</a>
+        <a href="#">Contact</a>
+      </div>
+      <div id="main">
+      <router-view />
+      </div>
   </div>
 </template>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -27,4 +74,66 @@
     }
   }
 }
+
+.sidenav {
+  height: 100%; /* Full-height: remove this if you want "auto" height */
+  width: 300px; /* Set the width of the sidebar */
+  position: fixed; /* Fixed Sidebar (stay in place on scroll) */
+  z-index: 1; /* Stay on top */
+  top: 0; /* Stay at the top */
+  left: 0;
+  background-color: #111; /* Black */
+  overflow-x: hidden; /* Disable horizontal scroll */
+  padding-top: 20px;
+  margin-top: 58px;
+}
+
+/* The navigation menu links */
+.sidenav a {
+  padding: 6px 8px 6px 16px;
+  text-decoration: none;
+  font-size: 25px;
+  color: #818181;
+  display: block;
+}
+
+/* When you mouse over the navigation links, change their color */
+.sidenav a:hover {
+  color: #f1f1f1;
+}
+
+#main{
+  margin-left: 350px;
+}
+
+@media screen and (max-width: 496px) {
+  .sidenav{width: 100px}
+  .sidenav {padding-top: 15px;}
+  .sidenav a {font-size: 18px;}
+  #main{margin-left: 120px}
+}
+@media screen and (max-width: 966px) {
+  .sidenav{width: 100px}
+  .sidenav {padding-top: 15px;}
+  .sidenav a {font-size: 18px;}
+  #main{margin-left: 120px}
+}
+// NAVBAR ======================================== 
+.topnav{
+    position: sticky;
+    background: black;
+    color: white;
+    padding-right: 40px;
+}
+span{
+    color: white;
+    
+}
+
+
+#cart{
+    padding-left: 5px;
+    display: inline;
+}
+
 </style>
--- restapi/src/views/Employees.vue
+++ restapi/src/views/Employees.vue
@@ -1,39 +1,126 @@
 <template>
   <div>
-      <ul>
-          <li v-for="(employee,i) in employees" :key="employee.id">
-              {{employees[i].name}}
-          </li>
-      </ul>
+    <div class="row">
+      <div class="col-lg-9 col-sm-9"></div>
+      <div class="col-lg-3 col-sm-3">
+        <button id="toggle" class="btn btn-outline-warning" @click="gridView()">
+          <img src="https://img.icons8.com/dotty/25/000000/activity-grid-2.png" />
+        </button>
+        <button id="toggle" class="btn btn-outline-warning" @click="listView()">
+          <img src="https://img.icons8.com/dotty/25/000000/ingredients-list.png" />
+        </button>
+      </div>
+    </div>
+    <div v-if="display == 'grid'">
+      <div class="row">
+        <div class="col-lg-4 col-sm-6" v-for="(employee,index) in employees" :key="employee.id">
+          <div class="card" style="width: 95%">
+            <div class="card-body">
+              <img class="card-img-top" id="img-1" v-bind:src="employee.image" alt="Card image cap" />
+              <h5 class="card-title">{{employee.name}}</h5>
+              <p class="card-text">₹{{employee.price}}</p>
+              <a class="btn btn-outline-warning" @click="addToCart(temployee,i)">
+                <p>add to cart</p>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div id="check" v-if="display == 'list'">
+      <div class="row">
+        <div class="col-lg-12 col-sm-12">
+          <table>
+            <th>Name</th>
+            <th>UserName</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Website</th>
+            <tr v-for="(employee,index) in employees" :key="employee.id">
+              <td>{{employee.name}}</td>
+              <td>{{employee.username}}</td>
+              <td>{{employee.email}}</td>
+              <td>{{employee.phone}}</td>
+              <td>{{employee.website}}</td>
+              <td></td>
+              <td>
+                <button class="btn btn-outline-info"> 
+                  <img src="https://img.icons8.com/pastel-glyph/14/000000/edit.png" />
+                </button>
+              </td>
+              <td></td>
+              <td>
+                <button class="btn btn-outline-info">
+                  <img src="https://img.icons8.com/metro/14/000000/delete-sign.png" />
+                </button>
+              </td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    </div>
+
+    <form action="">
+      <label for="">Name</label>
+      <input type="text" v-model="name">
+      <button v-on:click.prevent="post">add name</button>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            employees:null
-        }
+  data() {
+    return {
+      display: "grid",
+      employees: null,
+      name:null
+    };
+  },
+  mounted() {
+    this.loadData();
+  },
+  methods: {
+    listView() {
+      this.display = "list";
     },
-    mounted(){
-        this.loadData();
+    gridView() {
+      this.display = "grid";
     },
-    methods:{
-        loadData(){
-            var vm = this;
-            axios.get('https://jsonplaceholder.typicode.com/users')
-        .then((response)=>{
-            vm.employees = response.data
-          })
-        .catch((error)=>{
-            console.log(error);
+    loadData() {
+      var vm = this;
+      axios
+        .get("https://jsonplaceholder.typicode.com/users")
+        .then(response => {
+          this.employees = response.data;
         })
-        }
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    post(){
+      axios.post("https://jsonplaceholder.typicode.com/users", {
+      name: "abdan"
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
     }
-
-}
+  }
+};
 </script>
 
 <style>
+#check {
+  overflow: scroll;
+}
+#toggle {
+  width: 40px;
+  margin-left: 5px;
+}
 
 </style>
